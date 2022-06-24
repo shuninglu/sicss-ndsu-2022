6022b2974d149610f01e5251a3424eddeb573c40
Zoltan P. Majdik <zpm@zoltanpm.com>
2022-06-23 22:15:37 -0500

    update visualization files

--- Day 05/dataviz.R
+++ Day 05/dataviz.R
@@ -3,16 +3,20 @@
 library(tidyverse)
 library(RColorBrewer)
 library(maps)
+library(ggnetwork)
+library(gcookbook)
+library(networkD3)
+library(igraph)
+
 
 ca <- read_csv("https://raw.githubusercontent.com/ScienceParkStudyGroup/r-lesson-based-on-ohi-data-training/gh-pages/data/ca.csv") 
 
 p <- ggplot(data=ca,
             mapping = aes(x=year,
                           y=visitors))
-
+p
 # Why does it show nothing?
 
-p + geom_line()
 p + geom_point()
 p + geom_smooth()
 p + geom_point() + 
@@ -22,11 +26,11 @@ p + geom_point() +
 
 p <- ggplot(data=ca,
             mapping = aes(x=year,
-                          y=visitors,
+                          y=visitors, # reduce visitors var by 1000 
                           color=park_name))
 
-p + geom_smooth()
-p + geom_boxplot() # reduce visitors var by 1000
+p + geom_smooth() #+ facet_wrap(~park_name) show.legend = FALSE
+p + geom_boxplot() 
 
 
 p <- ggplot(data=ca,
@@ -37,7 +41,7 @@ p <- ggplot(data=ca,
 p + geom_point(alpha=0.4) + 
   xlab('Visitors') +
   ylab('Year') +
-  labs(color="Parks", size="Visitors (x1000)") +
+  labs(color="Parks", size="Visitors (x1000)")
   #scale_color_hue(h=c(0,360), c=200, l=50) # Remember to use scale_fill_hue for shapes
   #scale_color_manual(values = c('red', 'blue', 'green', 'purple', 'yellow', 'black', 'magenta', 'gray', 'green'))
   #scale_color_brewer(type="div", palette = 1, aesthetics = "color")
@@ -92,3 +96,38 @@ p <- ggplot(data=midwest_merged, aes(x=long, y=lat, group=group, fill=percbelowp
 p + 
   geom_polygon(color="gray50")
   #scale_fill_distiller(palette = "RdYlBu")
+
+
+# Networks
+madmen_relations <- madmen
+
+p <- simpleNetwork(madmen_relations)
+p
+# saveWidget() lets you save this as a html file
+# Or use igraph:
+network <- graph_from_data_frame(madmen_relations, directed=F)
+network #tweak directed variable
+plot(network)
+
+
+
+# Sankey
+links <- madmen
+links$value <- 1
+links <- links[links$Name1 < links$Name2, ]  # Removes reciprocal links. Try without!
+
+
+nodes <- data.frame(name=c(madmen$Name1, madmen$Name2)) %>% 
+                      unique()
+
+links$IDName1 <- match(links$Name1, nodes$name)-1 # Needs to be zero-indexed. No idea why
+links$IDName2 <- match(links$Name2, nodes$name)-1
+
+p <- sankeyNetwork(Links = links, 
+                   Nodes = nodes, 
+                   Source="IDName1", 
+                   Target="IDName2", 
+                   Value="value", 
+                   NodeID="name",
+                   sinksRight=TRUE) 
+p
